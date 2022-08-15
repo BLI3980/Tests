ac8b2b00e07649cdf80c9def5c896a064ba4378f
Boris Li <liba8936@protonmail.com>
2022-08-15 15:04:14 +0400

    Another commit prior to forder rename

---
+++ Reverse_Array/Program.cs
@@ -0,0 +1,10 @@
+﻿// Task: A number that is input from console, needs to be reversed, i.e. all digits to be written in reverse order.
+// Example: 12345 to become 54321
+
+Console.WriteLine("Enter your number: ");
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+
+//1. create an array
+//2. find out the number of digits in the number
+//3. fill out the array with numbers
+
