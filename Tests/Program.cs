48c10a70e8ce2afba2e90a9f852aa338e9971812
Boris Li <liba8936@protonmail.com>
2022-08-20 23:49:08 +0400

    sequential commit

---
+++ Tests/Program.cs
@@ -0,0 +1,14 @@
+﻿// double max=-10,min=10;
+
+//             Random rand = new Random();
+//             for (int i = 0; i < 50; i++)
+//             {
+// Console.WriteLine("value= " + Math.Round((rand.NextDouble() * (max - min) + min),2));
+// }
+
+
+for (int i = 0; i < 50; i++)
+{
+    double a = new Random().NextDouble();
+    Console.WriteLine(a);
+}
